c077730d06409960493b52ee5f3ae3e66b04bdcf
Randy <117179176+rgriggs0072@users.noreply.github.com>
2023-04-19 19:25:30 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ def transform_data():
     df_melted['Yes/No'] = df_melted['Yes/No'].apply(lambda x: 'Yes' if x == 1 else ('No' if pd.isna(x) else '*'))
 
     # Write the melted data to a CSV file
-    df_melted.to_csv('C:/Users/rgrig/Desktop/Randy_Spreadsheets/Distribution_Grid/Distribution_Grids/TRANSPOSED/SAVEMART_DISTRO_GRID_RGSR.csv', index=False, encoding='utf-8')
+    df_melted.to_csv('https://github.com/rgriggs0072/streamlit-example/blob/master/SAVEMART_DISTRO_GRID_RGSR_1.csv', index=False, encoding='utf-8')
 
     # Display the transformed data in Streamlit
     st.write(df_melted)
