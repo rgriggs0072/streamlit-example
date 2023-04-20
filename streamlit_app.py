15a1dc1f2f34ddc2a0161cc5d82c9f7713d9ac2d
Randy <117179176+rgriggs0072@users.noreply.github.com>
2023-04-19 18:39:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-st.header('Welcome to Delta Pacific's Spreadsheet Formatter')
+st.header('Welcome to Delta Pacifics Spreadsheet Formatter')
 st.write('hello I am the man')
 
 
