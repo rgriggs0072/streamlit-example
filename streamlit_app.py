5bca1b496553730b705d362834576051535c8066
Randy <117179176+rgriggs0072@users.noreply.github.com>
2023-04-19 19:02:54 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 def transform_data():
     # Read the data from the spreadsheet
     #df = pd.read_excel('C:/Users/rgrig/Desktop/Randy_Spreadsheets/Distribution_Grid/Distribution_Grids/SAVEMART_DISTRIBUTION_GRID_RGSR.xlsx')
-    df = pd.read_excel('https://github.com/rgriggs0072/streamlit-example/blob/master/SAVEMART_DISTRIBUTION_GRID_RGSR.xlsx', engine=openpyxl)
+    df = pd.read_excel('https://github.com/rgriggs0072/streamlit-example/blob/master/SAVEMART_DISTRIBUTION_GRID_RGSR.xlsx')
     # Get the store IDs from the first row
     store_ids = [x for x in df.columns[7:]]
 
